6f402b9b78303623bc711554c9e3748a24b52ce2
hraynaud <herby.raynaud@gmail.com>
2015-03-14 19:53:15 -0400

    Adds ability to filter list by checkboxes

--- app/assets/javascripts/angular/controllers/ideas_ctlr.js
+++ app/assets/javascripts/angular/controllers/ideas_ctlr.js
@@ -20,4 +20,45 @@ SparkApp.controller('ideasCtrl', function ($scope, Ideas) {
 
   $scope.countForStatus = Ideas.countsByStatus();
   $scope.countForLocation = Ideas.countsByLocation();
+
+  $scope.filter = { "status": {}, "location": {} };
+  $scope.toggle = function(type){
+     alert(type);
+  };
+
+
+  $scope.filterByProperties = function (idea) {
+    // Use this snippet for matching with AND
+    var matchesAND = true;
+    for (var prop in $scope.filter) {
+      if (noSubFilter($scope.filter[prop])) continue;
+      if (!$scope.filter[prop][idea[prop]]) {
+        matchesAND = false;
+        break;
+      }
+    }
+    return matchesAND;
+    /**/
+    /*
+    // Use this snippet for matching with OR
+    var matchesOR = true;
+    for (var prop in $scope.filter) {
+    if (noSubFilter($scope.filter[prop])) continue;
+    if (!$scope.filter[prop][wine[prop]]) {
+    matchesOR = false;
+    } else {
+    matchesOR = true;
+    break;
+    }
+    }
+    return matchesOR;
+    /**/
+  };
+
+    function noSubFilter(subFilterObj) {
+      for (var key in subFilterObj) {
+        if (subFilterObj[key]) return false;
+      }
+      return true;
+    }
 });
