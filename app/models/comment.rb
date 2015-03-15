b2f7b3f4fa408336f62f9663fd35a7fa7001c460
hraynaud <herby.raynaud@gmail.com>
2015-03-15 00:19:06 -0400

    Fixes son export

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ActiveRecord::Base
-  belongs_to :idea, , counter_cache: true
+  belongs_to :idea, counter_cache: true
   belongs_to :user
 end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,10 +1,11 @@
 class Idea < ActiveRecord::Base
   belongs_to :user
+	has_many :comments
 	enum status: [:considering, :planned, :in_progress, :completed, :rejected]
 
 	def self.json_ify
 		all.map.as_json( include:{
-			[user: {only: [:first_name, :last_name, :office_id] }
+			user: {only: [:first_name, :last_name, :office_id] }
 		})
 	end
   
