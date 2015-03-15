eafad0e115c48ba281b8571f2a8f44541dd22d40
hraynaud <herby.raynaud@gmail.com>
2015-03-15 00:04:40 -0400

    As to json method for ideas

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,5 +1,11 @@
 class Idea < ActiveRecord::Base
   belongs_to :user
-
 	enum status: [:considering, :planned, :in_progress, :completed, :rejected]
+
+	def self.json_ify
+		all.map.as_json( include:{
+			[user: {only: [:first_name, :last_name, :office_id] }
+		})
+	end
+  
 end
