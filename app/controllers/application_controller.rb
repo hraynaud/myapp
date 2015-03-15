ca12ebc553db6d77f8ad1c97725002bce8ae1393
hraynaud <herby.raynaud@gmail.com>
2015-03-15 11:33:33 -0400

    Adds create route for ideas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,24 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
   before_action :authenticate_user!
+  before_action :set_csrf_cookie_for_ng
 
 
   # Overwriting the sign_out redirect path method
   def after_sign_out_path_for(resource_or_scope)
    new_user_session_path
   end
+
+
+  def set_csrf_cookie_for_ng
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
+
+	def verified_request?
+		super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
+	end
+
+	
 end
--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -6,9 +6,22 @@ def index
 		respond_with(@ideas)
 end
 
+ def create
+   @idea = Idea.create(idea_params)
+	 if @idea
+     render json: {"everything":"is alright"}
+   else
+		head :unprocessable_entity 
+	 end 
+ end
+
 
 def default_serializer_options
   {root: false}
+end
+
+def idea_params
+	params.require(:idea).permit(:body, :title, :offices, :location_type)
 end
 
 end
